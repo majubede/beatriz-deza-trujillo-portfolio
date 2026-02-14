f8123170b87971e95417b3f2fd3b7e687bfe8376
majubede <majubede@gmail.com>
2026-02-14 17:05:51 +0000

    Changed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,14 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-export default defineConfig({
-  base: "/beatriz-deza-trujillo-portfolio/",
-  plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-});
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/beatriz-deza-trujillo-portfolio/",
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
-    },
+    hmr: { overlay: false },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
@@ -30,3 +17,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
